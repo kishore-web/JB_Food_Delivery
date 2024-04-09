7a80e61c291cc24c499dc5232609eef5b4521b1d
Kishore Ch <kishu.559@gmail.com>
2024-04-09 22:03:46 +0530

    restaurant data displayed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,22 @@
-import RestaurantSection from "./RestaurantSection";
+import { useEffect, useState } from "react";
+import { SWIGGY_API } from "../utils/constant";
+import RestCard from "./RestCard";
 const Body = () => {
+  const [restData, setRestData] = useState([]);
+  const fetchData = async () => {
+    const getdata = await fetch(SWIGGY_API);
+    const json = await getdata.json();
+    setRestData(
+      json.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    console.log(
+      json.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <section className="my-5 space-y-4">
       <div className="border-4">
@@ -8,13 +25,20 @@ const Body = () => {
       </div>
       <div className="border-4">
         <h2>Top restaurants</h2>
-
         <p>Slider</p>
       </div>
       <div className="border-4">
+        {restData.map((item) => (
+          <div key={item?.info?.id}>
+            <h4>{item?.info?.name}</h4>
+            <span>{item?.info?.avgRatingString}</span>{" "}
+            <span>{item?.info?.sla.slaString}</span>
+            <p>{item?.info?.cuisines.join(", ")}</p>
+          </div>
+        ))}
         <h2>Rrestaursnts with online food delivery</h2>
         <div> filter section </div>
-        <RestaurantSection />
+        <RestCard />
       </div>
     </section>
   );
